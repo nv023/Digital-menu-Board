bfc765893056621a1f89e66cc9e37374961f9417
naresh <naresh.das@vertitect.com>
2025-09-05 15:39:51 +0530

    second commit

--- js/custom.js
+++ js/custom.js
@@ -17,14 +17,26 @@ function addToCart(name, price) {
  
   // Product details (you can make this dynamic later)
   let product = {
-    name: "Mexican Grilled Chicken & Cheese Burger + Fries (M)",
+    name: "Warm & Comforting Hot Milk",
     price: 218,
+    price1: 350,
+    price2: 299,
+    price3: 150,
+    price4: 99,
+    price5: 49,
+    price6: 229,
     qty: 1
   };
 
   function updateModal() {
-    document.getElementById("qty").innerText = product.qty.toString().padStart(2, "0");
-    document.getElementById("grandTotal").innerText = product.price * product.qty;
+    // document.getElementById("qty").innerText = product.qty.toString().padStart(2, "0");
+    // document.getElementById("grandTotal").innerText = product.price * product.qty;
+    document.getElementById("grandTotal1").innerText = product.price1 * product.qty;
+    document.getElementById("grandTotal2").innerText = product.price2 * product.qty;
+    document.getElementById("grandTotal3").innerText = product.price3 * product.qty;
+    document.getElementById("grandTotal4").innerText = product.price4 * product.qty;
+    document.getElementById("grandTotal5").innerText = product.price5 * product.qty;
+    document.getElementById("grandTotal6").innerText = product.price6 * product.qty;
   }
 
   function increaseQty() {
